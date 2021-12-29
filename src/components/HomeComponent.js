b806944642f3a36ed604cd1117242867f200d8cd
Rachel Nurmi <rachelnurmi91@gmail.com>
2021-12-28 20:09:41 -0600

    Created Components: Header, Home & Footer. Created Router and rendered the components.

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home(props) {
+    return (
+        <div className="container">
+            <h2>If you see this...</h2>
+            <h4>home is working.</h4>
+        </div>
+    );
+}
+
+export default Home;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,3 +1,33 @@
-import React from 'react';
-import Apps from ''
+import React, { Component } from 'react';
+import Header from './HeaderComponent';
+import Home from './HomeComponent';
+import Footer from './FooterComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        const HomePage = () => {
+            return (
+                <Home />
+            );
+        }
+
+        return(
+            <div>
+                <Header />
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Redirect to='/home'/>
+                </Switch>
+                <Footer />
+            </div>
+        );
+    }
+}
+
 export default Main;
