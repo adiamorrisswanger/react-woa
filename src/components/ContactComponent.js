c58125d49b275389140fef2a032eb2b8695599bc
Rachel Nurmi <rachelnurmi91@gmail.com>
2022-01-01 17:20:29 -0600

    Controlled Form Added.

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,58 +1,175 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Button, Form, FormFeedback, FormGroup, Label, Input, Col } from 'reactstrap';
 
-function Contact (props) {
-    return(
-        <div className="container contact-div">
-            <h2>Contact Us</h2>
-            <hr />
-            <div className="contact-container">
-                <form>
-                    <div className="form-row">
-                        <div className="form-group col-md-6">
-                            <label for="firstName">First Name</label>
-                            <input type="text" className="form-control" id="firstName" name="firstName" placeholder="Jane" />
-                        </div>
-                        <div className="form-group col-md-6">
-                            <label for="lastName">Last Name</label>
-                            <input type="text" className="form-control" id="lastName" name="lastName" placeholder="Doe" />
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <label for="inputEmail">Address</label>
-                        <input type="email" className="form-control" id="inputEmail" name="emailAddress"
-                            placeholder="janedoe@gmail.com" />
-                    </div>
-                    <div className="form-group">
-                        <label for="inputNumber">Phone Number</label>
-                        <input type="phone" className="form-control" id="inputNumber" name="phoneNumber"
-                            placeholder="xxx-xxx-xxxx" />
-                    </div>
-                    <fieldset className="form-group">
-                        <div className="row">
-                            <legend className="col-form-label col-sm-4 pt-0 label">Preferred Contact Method</legend>
-                            <div className="col">
-                                <div className="form-check form-check-inline">
-                                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1"
-                                        value="option1" />
-                                    <label className="form-check-label" for="inlineRadio1" name="contactByEmail">Email</label>
-                                </div>
-                                <div className="form-check form-check-inline">
-                                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2"
-                                        value="option2" />
-                                    <label className="form-check-label" for="inlineRadio2" name="contactByPhone">Phone</label>
+class Contact extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            phoneNum: '',
+            contactType: 'By Phone',
+            message: '',
+            touched: {
+                firstName: false,
+                lastName: false,
+                phoneNum: false,
+                email: false
+            }
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    validate(firstName, lastName, phoneNum, email) {
+
+        const errors = {
+            firstName: '',
+            lastName: '',
+            phoneNum: '',
+            email: '',
+        };
+
+        if (this.state.touched.firstName) {
+            if (firstName.length < 2) {
+                errors.firstName = 'First name must be at least 2 characters.';
+            } else if (firstName.length > 15) {
+                errors.firstName = 'First name must be 15 or less characters.';
+            }
+        }
+
+        if (this.state.touched.lastName) {
+            if (lastName.length < 2) {
+                errors.lastName = 'Last name must be at least 2 characters.';
+            } else if (lastName.length > 15) {
+                errors.lastName = 'Last name must be 15 or less characters.';
+            }
+        }
+
+        const reg = /^\d+$/;
+        if (this.state.touched.phoneNum && !reg.test(phoneNum)) {
+            errors.phoneNum = 'The phone number should contain only numbers.';
+        }
+
+        if (this.state.touched.email && !email.includes('@')) {
+            errors.email = 'Email should contain a @';
+        }
+
+        return errors;
+    }
+
+    handleBlur = (field) => () => {
+        this.setState({
+            touched: {...this.state.touched, [field]: true}
+        });
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const name = target.name;
+        const value = target.value;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(event) {
+        console.log('Current state is: ' + JSON.stringify(this.state));
+        alert('Current state is: ' + JSON.stringify(this.state));
+        event.preventDefault();
+    }
+
+    render(){
+
+        const errors = this.validate(this.state.firstName, this.state.lastName, this.state.phoneNum, this.state.email); 
+
+        return(
+            <div className="container contact-div">
+                <h2>Contact Us</h2>
+                <hr />
+                <div className="contact-container">
+                    <Form onSubmit={this.handleSubmit}>
+
+                    <FormGroup row>
+                                <Label htmlFor="firstName" md={2}>First Name</Label>
+                                <Col md={10}>
+                                    <Input type="text" id="firstName" name="firstName"
+                                        placeholder="First Name"
+                                        value={this.state.firstName}
+                                        invalid={errors.firstName}
+                                        onBlur={this.handleBlur("firstName")}
+                                        onChange={this.handleInputChange} />
+                                    <FormFeedback>{errors.firstName}</FormFeedback>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="lastName" md={2}>Last Name</Label>
+                                <Col md={10}>
+                                    <Input type="text" id="lastName" name="lastName"
+                                        placeholder="Last Name"
+                                        value={this.state.lastName}
+                                        invalid={errors.lastName}
+                                        onBlur={this.handleBlur("lastName")}
+                                        onChange={this.handleInputChange} />
+                                    <FormFeedback>{errors.lastName}</FormFeedback>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="phoneNum" md={2}>Phone</Label>
+                                <Col md={10}>
+                                    <Input type="tel" id="phoneNum" name="phoneNum"
+                                        placeholder="Phone number"
+                                        value={this.state.phoneNum}
+                                        invalid={errors.phoneNum}
+                                        onBlur={this.handleBlur("phoneNum")}
+                                        onChange={this.handleInputChange} />
+                                    <FormFeedback>{errors.phoneNum}</FormFeedback>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label htmlFor="email" md={2}>Email</Label>
+                                <Col md={10}>
+                                    <Input type="email" id="email" name="email"
+                                        placeholder="Email"
+                                        value={this.state.email}
+                                        invalid={errors.email}
+                                        onBlur={this.handleBlur("email")}
+                                        onChange={this.handleInputChange} />
+                                    <FormFeedback>{errors.email}</FormFeedback>
+                                </Col>
+                            </FormGroup>
+
+                        <fieldset className="form-group">
+                            <div className="row">
+                                <legend className="col-form-label col-sm-4 pt-0 label">Preferred Contact Method</legend>
+                                <div className="col">
+                                    <Input type="select" name="contactType"
+                                        value={this.state.contactType}
+                                        onChange={this.handleInputChange}>
+                                        <option>By Phone</option>
+                                        <option>By Email</option>
+                                    </Input>
                                 </div>
                             </div>
+                        </fieldset>
+
+                        <div className="form-group">
+                            <Label htmlFor="message">Message</Label>
+                            <Input type="textarea" id="message" name="message" rows="5"
+                            value={this.state.message}
+                            onChange={this.handleInputChange}></Input>
                         </div>
-                    </fieldset>
-                    <div className="form-group">
-                        <label for="inputMessage">Message</label>
-                        <textarea className="form-control" id="inputMessage" name="theMessage" rows="5"></textarea>
-                    </div>
-                    <button type="submit" className="btn btn-green text-white">Submit</button>
-                </form>
+
+                        <Button type="submit" className="btn btn-green text-white">Submit</Button>
+                    </Form>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Contact;
