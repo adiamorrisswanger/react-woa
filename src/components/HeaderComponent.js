c68de7e43deddae9d4b958d6eaa47226a6007bc8
Adia Morris Swanger <adiamorrisswanger@gmail.com>
2022-02-06 14:47:42 -0600

    Login Button Part 1

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -15,6 +15,7 @@ class Header extends Component {
         };
 
         this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
     }
 
 
@@ -22,6 +23,16 @@ class Header extends Component {
         this.setState({
             isNavOpen: !this.state.isNavOpen
         });
+    }
+
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleLogin() {
+        alert(`Username: ${this.username.value} Password: ${this.password.value} Remember: ${this.remember.value}`);
     }
 
     render() {
@@ -65,9 +76,36 @@ class Header extends Component {
                                     </NavLink>
                                 </NavItem>
                             </Nav>
+                            <span className="navbar-text ml-auto">
+                                <Button onClick={this.toggleModal}>
+                                    <i className="fa fa-sign-in" /> Login
+                                </Button>
+                            </span>
                         </Collapse>
                     </div>
                 </Navbar>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username" innerRef={input => this.username = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password" innerRef={input => this.password = input} />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember" innerRef={input => this.remember = input} /> Remember Me
+                                </Label>
+                            </FormGroup>
+                                <Button type="submit" value="submit" color="primary">Login</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
+                
 
                 <Jumbotron fluid>
                     <CTA />
